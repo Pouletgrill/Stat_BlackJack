9d2677362f8e0ad168702aef39a3df0d2f3dfa2f
pouletgrill <xavierb.2006@hotmail.com>
2015-03-24 17:27:47 -0400

    PNG

---
+++ BlackJack/BlackJack/Carte.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Carte
+    {
+        Image cartePNG = null;
+
+        private Image GetImage()
+        {
+            return cartePNG;
+        }
+
+    }
+}
--- BlackJack/BlackJack/Form1.cs
+++ BlackJack/BlackJack/Form1.cs
@@ -10,11 +10,28 @@ using System.Windows.Forms;
 
 namespace BlackJack
 {
-   public partial class Form1 : Form
-   {
-      public Form1()
-      {
-         InitializeComponent();
-      }
-   }
+    public partial class Form1 : Form
+    {
+        List<Image>
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadImage();
+            MixCards();
+        }
+
+        private void LoadImage()
+        {
+
+        }
+
+        private void MixCards()
+        {
+
+        }
+    }
 }
