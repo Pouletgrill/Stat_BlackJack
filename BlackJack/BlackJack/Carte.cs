c4a5931a9bf1aba865e92b62e5f2309da4a43c5c
PouletGrill <xavierb.2006@hotmail.com>
2015-03-25 09:40:29 -0400

    PNG Rename et + :palm_tree:

--- BlackJack/BlackJack/Carte.cs
+++ BlackJack/BlackJack/Carte.cs
@@ -7,31 +7,42 @@ using System.Threading.Tasks;
 
 namespace BlackJack
 {
-    class Carte
-    {
-        Image cartePNG_ = null;
-        String path_ = null;
-        int valeur_;
+   class Carte
+   {
+      Image cartePNG_ = null;
+      String path_ = null;
+      int valeur_;
 
-        public Carte(String Path)
-        {
-            cartePNG_ = Image.FromFile(Path);
-            path_ = Path;
-        }
+      public Carte(String Path)
+      {
+         cartePNG_ = Image.FromFile(Path);
+         path_ = Path;
+      }
+      public Carte(String Path, int Valeur)
+      {
+         cartePNG_ = Image.FromFile(Path);
+         path_ = Path;
+         valeur_ = Valeur;
+      }
 
-        public Image GetImage()
-        {
-            return cartePNG_;
-        }
+      public void SetValeur(int Valeur)
+      {
+         valeur_ = Valeur;
+      }
 
-        public String GetPath()
-        {
-            return path_;
-        }
+      public Image GetImage()
+      {
+         return cartePNG_;
+      }
 
-        public int GetValeur()
-        {
-            return valeur_;
-        }
-    }
+      public String GetPath()
+      {
+         return path_;
+      }
+
+      public int GetValeur()
+      {
+         return valeur_;
+      }
+   }
 }
--- BlackJack/BlackJack/Form1.cs
+++ BlackJack/BlackJack/Form1.cs
@@ -11,82 +11,100 @@ using System.IO;
 
 namespace BlackJack
 {
-    public partial class Form1 : Form
-    {
-        List<Carte> paquet = new List<Carte>();
-        public Form1()
-        {
-            InitializeComponent();
-        }
+   public partial class Form1 : Form
+   {
+      List<Carte> paquet = new List<Carte>();
+      public Form1()
+      {
+         InitializeComponent();
+      }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            LoadImage();
-            MixCards();
-        }
+      private void Form1_Load(object sender, EventArgs e)
+      {
+         LoadImage();
+         MixCards();
+      }
 
-        private void LoadImage()
-        {
-            try
+      private void LoadImage()
+      {
+         try
+         {
+            //Création des objet cartes dans paquet
+            String[] file = Directory.GetFiles(@"Image_Carte", "*.PNG");
+            int valeur = 1;
+            for (int i = 0; i < 52; ++i)
             {
-                String[] array = Directory.GetFiles(@"Image_Carte", "*.PNG");
-                for (int i = 0; i < 54; ++i)
-                {
-                    paquet.Add(new Carte(array[i]));
-                }
-            }
-            catch (Exception es)
-            {
-               if (es.HResult == -2147024893)
+               paquet.Add(new Carte(file[i]));
+               if (valeur <= 9 && (i % 4) == 0)
                {
-                  MessageBox.Show("Erreur:\nLe document d'image \"Image_Carte\" n'est plus dans repertoire courent ou a été modifier.");
+                  valeur++;
                }
-               else
+               else if (valeur == 10 && (i % 16) == 0)
                {
-                  MessageBox.Show(es.Message);
+                  valeur++;
                }
-                this.Close();
+               paquet[i].SetValeur(valeur);
             }
-        }
-
-        private void MixCards()
-        {
-            List<Carte> Temp = new List<Carte>(paquet);
-            Random rnd = new Random();
-            int NbTemp;
-            paquet.Clear();
-            for (int i = 0; i < 54; i++)
+            ////////////////////////////////////////
+            string ccc = "ccc: ";
+            for (int i = 0; i < 52; i++)
             {
-                do
-                {
-                    NbTemp = rnd.Next(0, 54);
-                } while (paquet.Contains(Temp[NbTemp]));
-                paquet.Add(Temp[NbTemp]);
+               ccc += paquet[i].GetPath() + " #" + paquet[i].GetValeur().ToString() + "\n";
             }
-        }
+            MessageBox.Show(ccc);
+         }
+         catch (Exception es)
+         {
+            if (es.HResult == -2147024893)// si le document image est présent ou pas
+            {
+               MessageBox.Show("Erreur:\nLe document d'image \"Image_Carte\" n'est plus dans repertoire courent ou a été modifier.");
+            }
+            else
+            {
+               MessageBox.Show(es.Message);
+            }
+            this.Close();
+         }
+      }
 
-        private void RB_Humain1_and_RB_CPU1_CheckedChanged(object sender, EventArgs e)
-        {
-           if (RB_CPU1.Checked)
-           {
-              PN_AI_1.Enabled = true;
-           }
-           else
-           {
-              PN_AI_1.Enabled = false;
-           }
-        }
+      private void MixCards()
+      {
+         List<Carte> Temp = new List<Carte>(paquet);
+         Random rnd = new Random();
+         int NbTemp;
+         paquet.Clear();
+         for (int i = 0; i < 52; i++)
+         {
+            do
+            {
+               NbTemp = rnd.Next(0, 52);
+            } while (paquet.Contains(Temp[NbTemp]));
+            paquet.Add(Temp[NbTemp]);
+         }
+      }
 
-        private void RB_Humain2_and_RB_CPU2_CheckedChanged(object sender, EventArgs e)
-        {
-           if (RB_CPU2.Checked)
-           {
-              PN_AI_2.Enabled = true;
-           }
-           else
-           {
-              PN_AI_2.Enabled = false;
-           }
-        }
-    }
+      private void RB_Humain1_and_RB_CPU1_CheckedChanged(object sender, EventArgs e)
+      {
+         if (RB_CPU1.Checked)
+         {
+            PN_AI_1.Enabled = true;
+         }
+         else
+         {
+            PN_AI_1.Enabled = false;
+         }
+      }
+
+      private void RB_Humain2_and_RB_CPU2_CheckedChanged(object sender, EventArgs e)
+      {
+         if (RB_CPU2.Checked)
+         {
+            PN_AI_2.Enabled = true;
+         }
+         else
+         {
+            PN_AI_2.Enabled = false;
+         }
+      }
+   }
 }
