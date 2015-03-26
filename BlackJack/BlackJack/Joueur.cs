65e6c06ee57dc614bd0d220a10127a45c9ea6d47
pouletgrill <xavierb.2006@hotmail.com>
2015-03-26 16:36:15 -0400

    erwt

--- BlackJack/BlackJack/Joueur.cs
+++ BlackJack/BlackJack/Joueur.cs
@@ -6,50 +6,74 @@ using System.Threading.Tasks;
 
 namespace BlackJack
 {
-   class Joueur
-   {
-      int TotalPoint;
-      String Nom_;
-      int Cpu_Level_;
-      bool Joue_ = true;
+    class Joueur
+    {
+        int TotalPoint = 0;
+        String Nom_;
+        int Cpu_Level_ = -1;
+        bool Joue_ = true;
+        bool Compte_carte_ = false;
 
+        public Joueur(String Nom)
+        {
+            Nom_ = Nom;
+        }
+        public Joueur(int Cpu_Level, bool CompteCarte, String Nom)
+        {
+            Nom_ = Nom;
+            Cpu_Level_ = Cpu_Level;
+            Compte_carte_ = CompteCarte;
+        }
 
-      public Joueur(String Nom)
-      {
-         TotalPoint = 0;
-         Nom_ = Nom;
-         Cpu_Level_ = 0;
-      }
-      public Joueur(int Cpu_Level,String Nom)
-      {
-         TotalPoint = 0;
-         Nom_ = Nom;
-         Cpu_Level_ = Cpu_Level;
-      }
+        public int GetTotal()
+        {
+            return TotalPoint;
+        }
+        public string GetNom()
+        {
+            return Nom_;
+        }
+        public int GetCpuLevel()
+        {
+            return Cpu_Level_;
+        }
+        public void SetTotal(int Total)
+        {
+            TotalPoint = Total;
+        }
+        public void ArreteDeJouer()
+        {
+            Joue_ = false;
+        }
+        public bool JoueEncore()
+        {
+            return Joue_;
+        }
 
-      public int GetTotal()
-      {
-         return TotalPoint;
-      }
-      public string GetNom()
-      {
-         return Nom_;
-      }
-      public int GetCpuLevel()
-      {
-         return Cpu_Level_;
-      }
-      public void SetTotal(int Total)
-      {
-         TotalPoint = Total;
-      }
-      public void ArreteDeJouer()
-      {
-          Joue_ = false;
-      }
-      public bool JoueEncore()
-      {
-          return Joue_;
-      }
-   }
+        //si les conditions ne sont plus favorable, le joueur s'arrête de lui même
+        public int CalculerStat(List<Carte> paquet, int IndexCarte)
+        {
+            int Stats = 0;//    %
+            if (GetCpuLevel() > 0 && GetTotal() > 10)//si le total est trop bas ou le joueur n'est pas un CPU
+            {
+                if (GetCpuLevel() == 1)
+                {
+
+                }
+                else if (GetCpuLevel() == 2)
+                {
+
+                }
+                else if (GetCpuLevel() == 3)
+                {
+
+                }
+            }
+            else //ne retourne que les stats
+            {
+               
+            }
+            return Stats;
+        }
+    }
 }
